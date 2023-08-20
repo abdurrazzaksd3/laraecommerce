e6e8fabfccb7a1416cd3eaa4c01cd423d0038dd2
raza <abdurrazzakxe@gmail.com>
2023-08-20 22:26:21 +0600

    Display Wishlist Items using livewire

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,9 @@ Auth::routes();
 Route::get('/',[App\Http\Controllers\Frontend\FrontendController::class, 'index']);
 Route::get('/collections',[App\Http\Controllers\Frontend\FrontendController::class, 'categories']);
 Route::get('/collections/{category_slug}',[App\Http\Controllers\Frontend\FrontendController::class, 'products']);
-
 Route::get('/collections/{category_slug}/{product_slug}',[App\Http\Controllers\Frontend\FrontendController::class, 'productView']);
+
+Route::get('wishlist',[App\Http\Controllers\Frontend\WishlistController::class, 'index']);
 
 
 
