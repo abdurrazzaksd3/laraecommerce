f240263bd95aac95b55381fedc55b9dd97284d6f
raza <abdurrazzakxe@gmail.com>
2023-08-28 13:46:39 +0600

    remove / delete cart item using livewire

--- app/Http/Livewire/Frontend/Cart/CartShow.php
+++ app/Http/Livewire/Frontend/Cart/CartShow.php
@@ -127,6 +127,28 @@ class CartShow extends Component
             ]);
         }
     }
+
+    public function removeCartItem(int $cartId)
+    {
+        $cartRemoveData = Cart::where('user_id',auth()->user()->id)->where('id',$cartId)->first();
+        if($cartRemoveData)
+        {
+            $cartRemoveData->delete();
+            $this->emit('CartAddedUpdated');
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Cart Item Removed Successfully',
+                'type' => 'success',
+                'status' => 200
+            ]); 
+        }
+        else{
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Something Went Wrong!',
+                'type' => 'error',
+                'status' => 500
+            ]);
+        }
+    }
     public function render()
     {
         $this->cart = Cart::where('user_id',auth()->user()->id)->get();
