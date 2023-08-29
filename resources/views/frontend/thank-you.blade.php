6a5459705ed277b8cae30e5e1cc622efbb81fdee
raza <abdurrazzakxe@gmail.com>
2023-08-29 13:00:47 +0600

    Checkout & place order using livewire

---
+++ resources/views/frontend/thank-you.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title','Thank You For Shopping')
+
+@section('content')
+
+
+    <div class="py-3 pyt-md-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="p-4 shadow bg-white">
+                        <h2>You Log</h2>
+                        <h4>Thank you for shopping with Lara-Ecommerce</h4>
+                        <a href="{{ url('/collections') }}" class="btn btn-primary">shop now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
