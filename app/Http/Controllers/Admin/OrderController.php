b1ea6e4c1b4b843ec26bb563097b3828f7027c7e
raza <abdurrazzakxe@gmail.com>
2023-08-30 13:52:31 +0600

    Generate order invoice as PDF

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -6,6 +6,7 @@ use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use App\Http\Controllers\Controller;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class OrderController extends Controller
 {
@@ -65,5 +66,24 @@ class OrderController extends Controller
             return redirect('admin/orders/'.$orderId)->with('message','Order Id not found');
         }
         
+    }
+
+
+    public function viewInvoice(int $orderId)
+    {
+        $order = Order::FindOrFail($orderId);
+        return view('admin.invoice.generate-invoice',compact('order'));
+    }
+
+    public function generateInvoice(int $orderId)
+    {
+        $order = Order::FindOrFail($orderId);
+
+        $data = ['order' => $order];
+
+        $pdf = Pdf::loadView('admin.invoice.generate-invoice', $data);
+
+        $todayDate = Carbon::now()->format('d-m-y');
+        return $pdf->download('invoice-'.$order->id.'-'.$todayDate.'pdf');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -99,6 +99,8 @@ Route::prefix('admin')->middleware('auth','isAdmin')->group(function(){
         Route::get('/orders/{orderId}', 'show');
         Route::put('/orders/{orderId}', 'updateOrderStatus');
         
+        Route::get('/invoice/{orderId}', 'viewInvoice');
+        Route::get('/invoice/{orderId}/generate', 'generateInvoice');
     });
 
    
